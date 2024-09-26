7f944c23ea232ffef27c80ef0b0b86dd2ee72daa
Marcio junior <marciojunior5872@gmail.com>
2024-09-25 23:48:24 -0300

    new feature

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -1,5 +1,6 @@
 import React, { CSSProperties, useEffect, useState } from 'react';
 import EarthAventure from './EarthComponent/EarthAventure';
+import { AiOutlineSound } from 'react-icons/ai'; // Ícone de som
 
 function HomeHero() {
   const [text, setText] = useState('');
@@ -44,13 +45,28 @@ function HomeHero() {
     zIndex: -1,
   };
 
+  const AudioAmong = new Audio('/assets/key.mp3');
+
+  const handleAudio = () => {
+    AudioAmong.currentTime = 0;
+    AudioAmong.play()
+  };
+
+
   return (
     <div className='flex xs:flex-col md:flex-row xs:h-[80vh] md:h-screen w-screen xs:pt-10 md:pt-0'>
       <div
         id='apresentation'
-        className='flex xs:w-full xs:h-[30vh] md:w-[50vw] md:h-full items-center justify-center md:justify-center overflow-x-hidden'
+        className='flex xs:w-full xs:h-[30vh] md:w-[50vw] md:h-full items-center justify-center overflow-x-hidden'
       >
-        <p className='text-slate-50 xs:text-left md:text-center xs:text-2xl md:text-4xl xs:w-[300px] md:w-[450px]'>{text}</p>
+        <p className='text-slate-50 xs:text-left xs:text-2xl md:text-4xl xs:w-[300px] md:w-[450px]'>{text}</p>
+        {/* Corrigido o onClick para chamar a função corretamente */}
+        <AiOutlineSound
+          color='#FFFF'
+          size={30}
+          className='-rotate-45 cursor-pointer'
+          onClick={handleAudio} // Aqui a função é chamada corretamente
+        />
       </div>
 
       <div
@@ -59,7 +75,7 @@ function HomeHero() {
       >
         <EarthAventure />
         <div
-          className="absolute w-50 xs:w-[150px] xs:h-[150px] lg:w-[300px] lg:h-[300px] xs:left-[50%] xs:top-[50%] lg:left-[75%] lg:top-[50%]
+          className="absolute xs:w-[150px] xs:h-[150px] md:w-[300px] md:h-[300px] xs:left-[50%] xs:top-[50%] md:left-[50%] md:top-[50%]
             xs:shadow-[0_0_150px_90px_rgba(30,58,138,0.8)] lg:shadow-[0_0_500px_150px_rgba(30,58,138,1.5)]"
           style={shadowStyle}
         ></div>
