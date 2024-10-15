728ad943558189f3d39a3f927911c0a5e9df4a40
Marcio junior <marciojunior5872@gmail.com>
2024-10-15 04:39:19 -0300

    merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< Updated upstream
-=======
 import { LanguageProvider } from './context/LanguageContext';
 import './i18n';
 import 'typeface-inter';
->>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
 
